d44b19677aa76d26d5a652820df990f44d94cdcf
praneeth77777 <66330949+praneeth77777@users.noreply.github.com>
2020-08-10 01:12:32 +0530

    Update main.dart

--- browse code/main.dart
+++ browse code/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(Myapp1());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Myapp1();
+  }
+}
 
